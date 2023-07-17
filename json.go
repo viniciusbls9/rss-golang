e9960d32d55bb9784992288dc9d6f77ac7611ed5
viniciusbls9 <beneditoloura@gmail.com>
2023-07-17 07:34:21 -0300

    feat: create respondWithError func

--- json.go
+++ json.go
@@ -6,6 +6,19 @@ import (
 	"net/http"
 )
 
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	if code > 499 {
+		log.Println("Responding with 5xx error:", message)
+	}
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, code, errResponse{
+		Error: message,
+	})
+}
+
 func respondWithJSON (w http.ResponseWriter, code int, payload interface{}) {
 	dat, err := json.Marshal(payload)
 
