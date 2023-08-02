4ea7df62f47dba213c165a218ee520d937b5bc67
viniciusbls9 <beneditoloura@gmail.com>
2023-08-02 06:53:41 -0300

    feat: finish feeds logic

--- models.go
+++ models.go
@@ -15,6 +15,27 @@ type User struct {
 	APIKey 		string `json:"api_key"`
 }
 
+type Feed struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string `json:"name"`
+	APIKey 		string `json:"api_key"`
+	Url				string `json:"url"`
+	UserID 		uuid.UUID `json:"user_id"`
+}
+
+func databaseFeedToFeed(dbFeed database.Feed) Feed {
+	return Feed {
+		ID: dbFeed.ID,
+		CreatedAt: dbFeed.CreatedAt,
+		UpdatedAt: dbFeed.UpdatedAt,
+		Name: dbFeed.Name,
+		Url: dbFeed.Url,
+		UserID: dbFeed.UserID,
+	}
+}
+
 func databaseUserToUser(dbUser database.User) User {
 	return User{
 		ID: dbUser.ID,
