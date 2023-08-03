dd44bf037a28e89dd9607136874c9f1d85e880ba
viniciusbls9 <beneditoloura@gmail.com>
2023-08-03 07:24:53 -0300

    feat: create feed follows funcs

--- main.go
+++ main.go
@@ -58,6 +58,8 @@ func main() {
 	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerUsersGet))
 
 	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
+	v1Router.Post("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
 
 	router.Mount("/v1", v1Router)
 
