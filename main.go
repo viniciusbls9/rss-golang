7b00f077c034ccb0a0ddc87a9d2bf585ae69aa08
viniciusbls9 <beneditoloura@gmail.com>
2023-08-07 08:01:09 -0300

    feat: create scraping func to get feeds

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -43,9 +44,12 @@ func main() {
 		log.Fatal("Can't connect to database")
 	}
 
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB: database.New(conn),
+		DB: db,
 	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
--- scraper.go
+++ scraper.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/viniciusbls9/rss-golang/internal/database"
@@ -15,6 +17,42 @@ func startScraping(
 	log.Printf("Scraping on %v goroutines every %s durantion", concurrency, timeBetweenRequest)
 	ticker := time.NewTicker(timeBetweenRequest)
 	for ; ; <-ticker.C {
-		
+		feeds, err := db.GetNextFeedsToFetch(
+			context.Background(),
+			int32(concurrency),
+		)
+		if err != nil {
+			log.Println("error fetching feeds:", err)
+			continue
+		}
+
+		wg := &sync.WaitGroup{}
+		for _, feed := range feeds {
+			wg.Add(1)
+
+			go scrapeFeed(db, wg, feed)
+		}
+		wg.Wait()
 	}
+}
+
+func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
+	defer wg.Done()
+
+	_, err := db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		log.Println("Error marking feed as fetched:", err)
+		return
+	}
+
+	rssFeed, err := urlToFeed(feed.Url)
+	if err != nil {
+		log.Println("Error fetching feed:", err)
+		return
+	}
+
+	for _, item := range rssFeed.Channel.Item {
+		log.Println("Found post:", item.Title, "on feed", feed.Name)
+	}
+	log.Printf("Feed %s collected %v post found", feed.Name, len(rssFeed.Channel.Item))
 }
